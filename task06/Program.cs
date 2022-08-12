a374ea2e3ad1d62a7cf84ecf177269b812ccb8b4
Olga <supernatural64@mail.ru>
2022-08-12 13:08:27 +0400

    task06

---
+++ task06/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число : ");
+int A = Convert.ToInt32(Console.ReadLine());
+if (A % 2 == 0)
+ 
+{
+    Console.Write("Введенное число является четным");
+    Console.Read();
+}
+ 
+ else
+ {
+    Console.Write("Введенное число нечетное");
+    Console.Read();
+ }
